95cb87c0adf85d3c8639784cf2c3dc57adb23977
Alexander Warnes <warnes.alexander@gmail.com>
2020-04-03 15:07:58 -0700

    Linting and start of routing structure.

--- src/Room.tsx
+++ src/Room.tsx
@@ -1,2 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { useParams } from 'react-router-dom';
 
+class Room extends Component {
+  constructor(props: any) {
+    super(props);
+    const { roomId } = useParams();
+  }
+  render() {
+    return <div />;
+  }
+}
+
+export default Room;
