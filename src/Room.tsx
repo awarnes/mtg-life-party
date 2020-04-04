751f2a87aca32b457f7c6276396ed064fb17c6b8
Alexander Warnes <warnes.alexander@gmail.com>
2020-04-04 04:05:52 -0700

    Good pass over player card.

--- src/Room.tsx
+++ src/Room.tsx
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { Component, useEffect, useState } from 'react';
 // MaterialUI Core
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles, Theme } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import IconButton from '@material-ui/core/CardActionArea';
+import Tooltip from '@material-ui/core/Tooltip';
 import Avatar from '@material-ui/core/Avatar';
 import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Fab from '@material-ui/core/Fab';
+import { red, orange, yellow, teal, lightBlue, indigo, deepPurple } from '@material-ui/core/colors';
 // Icons
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import BrushIcon from '@material-ui/icons/Brush';
+import CloseIcon from '@material-ui/icons/Close';
 // MTG Life Party
-import { RoomProps, Player } from './mtgLifeTypeHelpers';
+import { RoomProps } from './mtgLifeTypeHelpers';
 
 function getGathererURL(commanderName: string): string {
   const searchArray = commanderName.replace(/[^\w\s]/g, '').split(' ');
 
   return `https://gatherer.wizards.com/Pages/Search/Default.aspx?name=${searchArray.map((term: string): string => `|[${term}]`).join('')}`;
 }
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 const useStyles = makeStyles((theme: any) => ({
   container: {
@@ -54,6 +61,34 @@ const useStyles = makeStyles((theme: any) => ({
     width: theme.spacing(10),
     height: theme.spacing(10),
   },
+  red: {
+    color: theme.palette.getContrastText(red[500]),
+    backgroundColor: red[500],
+  },
+  orange: {
+    color: theme.palette.getContrastText(orange[500]),
+    backgroundColor: orange[500],
+  },
+  yellow: {
+    color: theme.palette.getContrastText(yellow[500]),
+    backgroundColor: yellow[500],
+  },
+  green: {
+    color: theme.palette.getContrastText(teal[500]),
+    backgroundColor: teal[500],
+  },
+  blue: {
+    color: theme.palette.getContrastText(lightBlue[500]),
+    backgroundColor: lightBlue[500],
+  },
+  indigo: {
+    color: theme.palette.getContrastText(indigo[500]),
+    backgroundColor: indigo[500],
+  },
+  violet: {
+    color: theme.palette.getContrastText(deepPurple[500]),
+    backgroundColor: deepPurple[500],
+  },
 }));
 
 class Room extends Component<RoomProps> {
@@ -69,21 +104,83 @@ class Room extends Component<RoomProps> {
   }
 }
 
-function PlayerCard(props: any) {
-  const classes = useStyles();
-
+function PlayerCard(props: any): any {
+  const classes: any = useStyles();
   const { player, reduceLife, increaseLife } = props;
 
-  const handleReduceClick = () => {
+  const [settingsOpen, setSettingsOpen] = useState(false);
+  const [cardColor, setCardColor] = useState(colors[~~(Math.random() * colors.length)]);
+
+  const baseTempChangeTimer = 2000;
+  const [tempChange, setTempChange] = useState(0);
+  const [tempChangeMS, setMilliseconds] = useState(baseTempChangeTimer);
+  const [tempChangeActive, setTempChangeActive] = useState(false);
+
+  function startTempChange(): void {
+    setTempChangeActive(true);
+  }
+
+  function resetTempTimer(): void {
+    setMilliseconds(baseTempChangeTimer);
+    setTempChangeActive(false);
+  }
+
+  function resetTempChange(): void {
+    resetTempTimer();
+    setTempChange(0);
+  }
+
+  useEffect(() => {
+    let interval: any = null;
+    if (tempChangeActive) {
+      if (tempChangeMS <= 0) {
+        resetTempChange();
+      }
+      interval = setInterval(() => {
+        setMilliseconds((tempChangeMS) => tempChangeMS - 100);
+      }, 100);
+    } else if (!tempChangeActive && tempChangeMS !== baseTempChangeTimer) {
+      clearInterval(interval);
+    }
+    return (): void => clearInterval(interval);
+  }, [tempChangeActive, tempChangeMS, resetTempChange]);
+
+  const handleSettingsToggle = (): void => {
+    setSettingsOpen(!settingsOpen);
+  };
+
+  const handleSetColor = (colorChoice: string): void => {
+    setCardColor(colorChoice);
+    setSettingsOpen(false);
+  };
+
+  const handleReduceClick = (): void => {
+    resetTempTimer();
+    setTempChange(tempChange - 1);
     reduceLife();
+    startTempChange();
   };
-  const handleIncreaseClick = () => {
+
+  const handleIncreaseClick = (): void => {
+    resetTempTimer();
+    setTempChange(tempChange + 1);
     increaseLife();
+    startTempChange();
   };
+
   return (
     <Card>
       <CardHeader
-        avatar={<Avatar aria-label="Player Name">{player.name[0].toUpperCase()}</Avatar>}
+        avatar={
+          <Avatar aria-label="Player Name" className={classes[cardColor]}>
+            {player.name[0].toUpperCase()}
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings" onClick={handleSettingsToggle}>
+            {settingsOpen ? <CloseIcon /> : <MoreVertIcon />}
+          </IconButton>
+        }
         title={player.name}
         subheader={
           <div>
@@ -107,29 +204,46 @@ function PlayerCard(props: any) {
           </div>
         }
       />
+      {settingsOpen ? (
+        <div>
+          <CardContent>
+            {colors.map((colorChoice: string) => (
+              <Fab key={colorChoice} className={classes[colorChoice]} onClick={(): void => handleSetColor(colorChoice)}>
+                <BrushIcon />
+              </Fab>
+            ))}
+          </CardContent>
+        </div>
+      ) : (
+        <div>
+          <CardContent className={classes.container}>
+            <IconButton className={classes.reduceButton} onClick={handleReduceClick}>
+              <RemoveIcon />
+            </IconButton>
+            <IconButton className={classes.increaseButton} onClick={handleIncreaseClick}>
+              <AddIcon />
+            </IconButton>
+            <Tooltip title={tempChange >= 0 ? `+${tempChange}` : tempChange} placement={tempChange < 0 ? 'left' : 'right'} open={tempChangeActive} arrow>
+              <TooltipWrappedAvatar aria-label="Player Life" className={[classes.lifeTotal, classes[cardColor]].join(' ')}>
+                {player.life}
+              </TooltipWrappedAvatar>
+            </Tooltip>
+          </CardContent>
 
-      <CardContent className={classes.container}>
-        <IconButton className={classes.reduceButton} onClick={handleReduceClick}>
-          <RemoveIcon />
-        </IconButton>
-        <IconButton className={classes.increaseButton} onClick={handleIncreaseClick}>
-          <AddIcon />
-        </IconButton>
-        <Avatar aria-label="Player Life" className={classes.lifeTotal}>
-          {player.life}
-        </Avatar>
-      </CardContent>
-
-      <ExpansionPanel>
-        <ExpansionPanelSummary>
-          <Typography>Summary for Panel</Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>Details for Panel</Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
+          <ExpansionPanel>
+            <ExpansionPanelSummary>
+              <Typography>Summary for Panel</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>Details for Panel</Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+        </div>
+      )}
     </Card>
   );
 }
+// eslint-disable-next-line
+const TooltipWrappedAvatar = React.forwardRef((props: any, ref: any) => <Avatar {...props} ref={ref} />);
 
 export default Room;
