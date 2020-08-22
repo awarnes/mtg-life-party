7bb6fb58364a91aa42bf9ff228fd187fcb8e30a0
Alexander Warnes <warnes.alexander@gmail.com>
2020-08-21 18:25:12 -0700

    Add await?

--- src/components/CardRulings.tsx
+++ src/components/CardRulings.tsx
@@ -3,8 +3,8 @@ import Typography from '@material-ui/core/Typography';
 
 import { getURI } from '../data/scryfall';
 
-export default async function CardRulings(props: { rulingsUri: string }) {
-  return await getURI(props.rulingsUri).map((ruling: any) => {
+export default async function CardRulings(props: { rulingsUri: string }): Promise<JSX.Element[]> {
+  return (await getURI(props.rulingsUri)).map((ruling: any) => {
     return (
       <div key={ruling.oracle_id}>
         <Typography>Date: {ruling.published_at}</Typography>
